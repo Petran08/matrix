436ea33657332b7afa471d6b1effea52a51c597d
Petran08 <petranraulfrancesco@gmail.com>
2025-09-30 09:16:22 +0300

    small changes

--- matrix/matrice.h
+++ matrix/matrice.h
@@ -3,11 +3,19 @@
 
 using namespace std;
 
+enum error {
+	ADDITION_ERROR,
+	MULTIPLICATION_ERROR,
+	DET_ERROR,
+	NONE
+};
+
 class matrice
 {
 public:
 	int n, m;// m/i nr linii, n/j nr coloane
-	double values[5][5] = { 0 };
+	double values[3][3] = { 0 };
+	error operationError = NONE;
 	
 	matrice(int linii = 3, int coloane = 3)
 	{
@@ -29,13 +37,35 @@ public:
 		cout << '\n';
 	}
 
+	double det()
+	{
+		if (m != n)
+		{
+			cout << "NOT POSSIBLE\n";
+			operationError = DET_ERROR;
+			return 0;
+		}
+		operationError = NONE;
+		if (m == 1)
+		{
+			return values[0][0];
+		}
+		else if (m == 2)
+		{
+			return values[0][0] * values[1][1] - values[0][1] * values[1][0];
+		}
+		else
+		{
+			return values[0][0] * values[1][1] * values[2][2] + values[1][0] * values[2][1] * values[0][2] + values[2][0] * values[0][1] * values[1][2] - values[0][2] * values[1][1] * values[2][0] - values[0][0] * values[1][2] * values[2][1] - values[0][1] * values[1][0] * values[2][2];
+		}
+	}
+
 	bool operator==(const matrice& other)
 	{
 		if (n != other.n || m != other.m)
 		{
 			return false;
 		}
-		bool valEqual = true;
 		for(int i = 0; i < m; i++) // linii
 		{
 			for (int j = 0; j < n; j++) //coloane
@@ -52,8 +82,10 @@ public:
 		if (n != other.n || m != other.m)
 		{
 			cout << "NOT POSSIBLE\n";
+			operationError = ADDITION_ERROR;
 			return 0;
 		}
+		operationError = NONE;
 		matrice sum(m, n);
 		for (int i = 0; i < m; i++) // linii
 		{
@@ -70,8 +102,10 @@ public:
 		if (n != other.n || m != other.m)
 		{
 			cout << "NOT POSSIBLE\n";
+			operationError = ADDITION_ERROR;
 			return 0;
 		}
+		operationError = NONE;
 		matrice diff;
 		for (int i = 0; i < m; i++) // linii
 		{
@@ -111,8 +145,27 @@ public:
 
 	matrice operator*(const matrice& other)
 	{
-		matrice prod(m, n);
-		prod.values[1][1] = 8;  // for testing
+		if (n != other.m)
+		{
+			cout << "NOT POSSIBLE\n";
+			operationError = MULTIPLICATION_ERROR;
+		}
+		operationError = NONE;
+		matrice prod(m, other.n);
+		
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < other.n; j++)
+			{
+				double val = 0;
+				for (int x = 0; x < n; x++)
+				{
+					val += values[i][x] * other.values[x][j];
+				}
+				prod.values[i][j] = val;
+			}
+		}
+		
 		return prod;
 	}
 };
--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -5,16 +5,44 @@ using namespace std;
 
 int main()
 {
-	matrice t1(2, 2), t2(2, 2);
+	matrice t1(3, 3), t2(3, 3), t3(3, 3), t4(2, 2);
 	t1.show();
 	t1.values[0][0] = 1;
-	t1.values[0][1] = 2;
-	t1.values[1][0] = 3;
-	t1.values[1][1] = 4;
+	t1.values[0][1] = 3;
+	t1.values[0][2] = -1;
+	t1.values[1][0] = 2;
+	t1.values[1][1] = 3;
+	t1.values[1][2] = 0;
+	t1.values[2][0] = -2;
+	t1.values[2][1] = 1;
+	t1.values[2][2] = 1;
 	t1.show();
 
-	t2 = t1 * 3;
-
 	t2.show();
+	t2.values[0][0] = -2;
+	t2.values[0][1] = 3;
+	t2.values[0][2] = 1;
+	t2.values[1][0] = 3;
+	t2.values[1][1] = 0;
+	t2.values[1][2] = 3;
+	t2.values[2][0] = 1;
+	t2.values[2][1] = 2;
+	t2.values[2][2] = 2;
+	t2.show();
+
+	t3 = t1 * t2;
+
+	t3.show();
+
+	cout << t2.det() << '\n';
+
+	t4.values[0][0] = 1;
+	t4.values[0][1] = 2;
+	t4.values[1][0] = 3;
+	t4.values[1][1] = 4;
+
+	cout << t4.det() << '\n';
+
+	return 0;
 }
 
